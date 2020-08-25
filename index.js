357c48ab6398972a80b8294b75df0b0848a6632f
Jan Kohlbach <mail@jankohlbach.dev>
2020-08-25 11:32:52 +0200

    exercise 3.4

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ app.get('/api/person/:id', (req, res) => {
   person ? res.json(person) : res.status(404).end();
 });
 
+app.delete('/api/person/:id', (req, res) => {
+  persons = persons.filter((person) => person.id !== Number(req.params.id));
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
